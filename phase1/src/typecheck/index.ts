8a663c84f31d00b1a658e5a370f0bb8d75fc3e8f
Raoul Duke <raould@gmail.com>
2026-01-15 18:00:36 -0800

    wip

--- phase1/src/typecheck/index.ts
+++ phase1/src/typecheck/index.ts
@@ -1,5 +1,6 @@
 import { CompilerContext } from "../api.js";
 import { TypeCheckerBase } from "t2-phase0";
+import type { Phase0Program } from "../ast/nodes.js";
 
 /**
  * Thin Phase1 TypeChecker that reuses Phase0's TypeCheckerBase.
@@ -7,5 +8,10 @@ import { TypeCheckerBase } from "t2-phase0";
 export class TypeChecker extends TypeCheckerBase {
   constructor(ctx: CompilerContext) {
     super(ctx);
+  }
+
+  // Explicitly accept Phase0Program to make the Phase1 -> Phase0 boundary clear
+  checkProgram(program: Phase0Program): ReturnType<TypeCheckerBase["checkProgram"]> {
+    return super.checkProgram(program as any);
   }
 }
