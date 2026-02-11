b5f1168500799712aa760de55895e09d306dab09
Raoul Duke <raould@gmail.com>
2026-02-11 09:26:28 -0800

    wip

--- stage1/index.ts
+++ stage1/index.ts
@@ -2,22 +2,25 @@ import { CharStream, CommonTokenStream } from "antlr4ng";
 import { Stage1Lexer } from "./Stage1Lexer";
 import { RawContext, BindingContext, CallContext, ExpressionContext, LambdaContext, LetStarContext, LiteralContext, ParamContext, ProgramContext, Stage1Parser, StatementContext } from "./Stage1Parser";
 import fs from "node:fs";;
+let dbg = (...msgs) => {
+  console.log(...msgs);;
+};
 let parseString = (tokenText) => {
-  //console.log('+++parseString', tokenText);;
+  dbg('+++parseString', tokenText);;
   const raw = tokenText;
       const inner = raw.slice(1, -1);
       // antlr and json escapes are the same (?!)
       return JSON.parse('"' + inner.replace(/"/g, '\\"') + '"');;
 };
 let astProgram = (ctx) => {
-  //console.log('+++astProgram', Object.keys(ctx));;
+  dbg('+++astProgram', Object.keys(ctx), ctx.getText());
   return {
           tag: "program",
           body: ctx.statement().map(astStatement),
       };;
 };
 let astStatement = (ctx) => {
-  //console.log('+++astStatement', Object.keys(ctx));;
+  dbg('+++astStatement', Object.keys(ctx), ctx.getText());;
   if (ctx.letStar()) {
           return astLetStar(ctx.letStar()!);
       }
@@ -30,10 +33,13 @@ let astStatement = (ctx) => {
       if (ctx.if()) {
           return astIf(ctx.if());
       }
+      if (ctx.while()) {
+        return astWhile(ctx.while());
+      }
       return astExpression(ctx.expression()!);;
 };
 let astIf = (ctx) => {
-  //console.log('+++astIf', Object.keys(ctx));;
+  dbg('+++astIf', Object.keys(ctx), ctx.getText());;
   const test = astExpression(ctx.expression());
         const ifthen = astStatement(ctx.statement(0));
         const ctxElse = ctx.statement(1);
@@ -41,10 +47,13 @@ let astIf = (ctx) => {
         return { tag: 'if', test, ifthen, ifelse };;
 };
 let astWhile = (ctx) => {
-  //console.log('+++astWhile', Object.keys(ctx));;
+  dbg('+++astWhile', Object.keys(ctx), ctx.getText());;
+  const test = astExpression(ctx.expression());
+        const body = astStatement(ctx.statement());
+        return { tag: 'while', test, body };;
 };
 let astLetStar = (ctx) => {
-  //console.log('+++astLetStar', Object.keys(ctx));;
+  dbg('+++astLetStar', Object.keys(ctx), ctx.getText());;
   const bindings = ctx
       .binding()
       .map((b: BindingContext) => {
@@ -56,7 +65,7 @@ let astLetStar = (ctx) => {
       return { tag: 'let*', bindings, body };;
 };
 let astLambda = (ctx) => {
-  //console.log('+++astLambda', Object.keys(ctx));;
+  dbg('+++astLambda', Object.keys(ctx), ctx.getText());;
   const params = ctx
       .fnSignature()
       .param()
@@ -65,7 +74,7 @@ let astLambda = (ctx) => {
       return { tag: "lambda", params, body };;
 };
 let astExpression = (ctx) => {
-  //console.log('+++astExpression', Object.keys(ctx));;
+  dbg('+++astExpression', Object.keys(ctx), ctx.getText());;
       if (ctx.literal()) {
           return astLiteral(ctx.literal()!);
       }
@@ -84,7 +93,7 @@ let astExpression = (ctx) => {
       throw new Error("Unknown expression node");;
 };
 let astCall = (ctx) => {
-  //console.log('+++astCall', Object.keys(ctx));;
+  dbg('+++astCall', Object.keys(ctx), ctx.getText());;
   const exprs = ctx
       .expression()
       .map(astExpression);
@@ -95,7 +104,7 @@ let astCall = (ctx) => {
       };;
 };
 let astLiteral = (ctx) => {
-  //console.log('+++astLiteral', Object.keys(ctx));;
+  dbg('+++astLiteral', Object.keys(ctx), ctx.getText());;
   if (ctx.NUMBER()) return { tag: "literal", value: Number(ctx.NUMBER()!.getText()) };
       if (ctx.STRING()) return { tag: "literal", value: parseString(ctx.STRING()!.getText()) }; 
       if (ctx.BOOLEAN()) return { tag: "literal", value: ctx.BOOLEAN()!.getText() === "true" }; 
@@ -104,18 +113,18 @@ let astLiteral = (ctx) => {
       throw new Error("Unknown literal");;
 };
 let astRaw = (ctx) => {
-  //console.log('+++astRaw', Object.keys(ctx));;
+  dbg('+++astRaw', Object.keys(ctx), ctx.getText());;
   return {
           tag: "raw",
           code: parseString(ctx.STRING()!.getText())
       };;
 };
 let emitProgram = (node) => {
-  //console.log('+++emitProgram', Object.keys(node));;
+  dbg('+++emitProgram', Object.keys(node));;
   return node.body.map(emitStmt).join('\n');;
 };
 let emitIf = (node) => {
-  //console.log('+++if', Object.keys(node));;
+  dbg('+++if', Object.keys(node));;
   const lines=[];
         lines.push(`if (${emitExpr(node.test)}) {`);
         lines.push(indent(emitStmt(node.ifthen)));
@@ -127,8 +136,16 @@ let emitIf = (node) => {
         }
         return lines.join('\n');;
 };
+let emitWhile = (node) => {
+  dbg('+++while', Object.keys(node));;
+  const lines=[];
+        lines.push(`while (${emitExpr(node.test)}) {`);
+        lines.push(indent(emitStmt(node.body)));
+        lines.push("}");
+        return lines.join('\n');;
+};
 let emitLetStar = (node) => {
-  //console.log('+++emitStar', Object.keys(node));;
+  dbg('+++emitStar', Object.keys(node));;
   const lines = [];
     for (const b of node.bindings) {
       if (b.init) {
@@ -143,7 +160,7 @@ let emitLetStar = (node) => {
     return lines.join('\n');;
 };
 let emitExpr = (expr) => {
-  //console.log('+++emitExpr', Object.keys(expr));;
+  dbg('+++emitExpr', Object.keys(expr));;
   switch (expr.tag) {
       case 'literal':     return JSON.stringify(expr.value);
       case 'identifier':  return expr.name;
@@ -151,11 +168,12 @@ let emitExpr = (expr) => {
       case 'lambda':      return emitLambda(expr);
       case 'raw':         return expr.code;
       case 'if':          return emitIf(expr);
-      default: throw new Error("unhandled expr.tag >" + expr.tag + "<");
+      case 'while':       return emitWhile(expr);
+      default: throw new Error("Unhandled expr.tag >" + expr.tag + "<");
     };
 };
 let emitLambda = (node) => {
-  //console.log('+++emitLambda', Object.keys(node));;
+  dbg('+++emitLambda', Object.keys(node));;
   const params = node.params.join(', ');
     const body = node.body.map(emitStmt).join('\n');
     return `(${params}) => {\n${indent(body)}\n}`;;
@@ -170,7 +188,7 @@ let emitStmt = (stmt) => {
     };
 };
 let indent = (text) => {
-  //console.log('+++emitIndent', text);;
+  dbg('+++emitIndent', text);;
   return text.split('\n').map(line => '  ' + line).join('\n');;
 };
 let main = () => {
@@ -180,7 +198,7 @@ let main = () => {
   const tokenStream = new CommonTokenStream(lexer);
   const parser = new Stage1Parser(tokenStream);
   const tree = parser.program();
-  //console.log("???", tree.toStringTree(parser));
+  console.log("???", tree.toStringTree(parser));
   const ast = astProgram(tree);
   console.log(emitProgram(ast));;
 };
